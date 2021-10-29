e3837a7c54dee03eb6f766f2d5aa77c82250c328
Ekomobong Wisdom <61995296+wisdomn200@users.noreply.github.com>
2021-10-30 00:05:26 +0100

    Add files via upload
    A simple yet stylish clock with a date i built with html, CSS and JavaScript. I imported a few things like the fonts and the background image. i named it after my favorite DC character, Batman. hence the name 'Bat watch.'   I really hope it helps him fight crime and protect gotham city.

---
+++ logic.js
@@ -0,0 +1,43 @@
+ function startTime() { 
+    var today=new Date(); 
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    var ampm = "";
+    m = checkTime(m); 
+
+    if (h > 12) {
+    	h = h - 12;
+    	ampm = " PM";
+    } else if (h == 12){
+        h = 12;
+    	ampm = " AM";
+    } else if (h < 12){
+        ampm = " AM";
+    } else {
+        ampm = "PM";
+    };
+  
+  if(h==0) {
+    h=12;
+  }
+    
+    document.getElementById('display').innerHTML = h+":"+m+ampm;
+    var t = setTimeout(function(){startTime()},500);
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+
+//date
+function startDate() {
+  var d = new Date();
+  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  document.getElementById("date").innerHTML = days[d.getDay()]+" | "+[d.getMonth()+1]+"/"+d.getDate()+"/"+d.getFullYear();
+}
+
+
+
